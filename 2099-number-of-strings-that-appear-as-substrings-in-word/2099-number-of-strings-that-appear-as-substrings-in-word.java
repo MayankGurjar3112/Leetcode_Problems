d1afd49e075646e3d2f600625b353bab164eee99
Mayank Gurjar <111596779+MayankGurjar3112@users.noreply.github.com>
2026-06-29 15:34:35 +0530

    2099-NUMBER-OF-STRINGS-THAT-APPEAR-AS-SUBSTRINGS-IN-WORD Stats: Time: 1 ms (75.75%), Space: 43.5 MB (19.94%) - LeetHub

--- 2099-number-of-strings-that-appear-as-substrings-in-word/2099-number-of-strings-that-appear-as-substrings-in-word.java
+++ 2099-number-of-strings-that-appear-as-substrings-in-word/2099-number-of-strings-that-appear-as-substrings-in-word.java
@@ -1,12 +1,9 @@
 class Solution {
     public int numOfStrings(String[] patterns, String word) {
-        word = " ".concat(word).concat(" ");
         int count = 0;
-        for (String s : patterns) {
-            String[] temp = word.split(s);
-            if (temp.length > 1)
+        for (String s : patterns) 
+            if (word.contains(s))
                 count++;
-        }
         return count;
     }
 }
